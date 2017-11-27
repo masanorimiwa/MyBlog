6ffdcd942eb7d0042ba24e497d49bae78e54e196
masanorimiwa <miwamasanori0805@gmail.com>
2017-11-27 05:24:59 +0000

    Add weight

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171112130904) do
+ActiveRecord::Schema.define(version: 20171127052256) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20171112130904) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "weight"
   end
 
 end
